0e15c6dc3b7487d11bcb2df546dbaa94a9edf83e
Frederick-S <mao_xiaodan@hotmail.com>
2023-01-22 09:47:42 +0800

    add sentry to capture errors

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ting-app/ting-task-nhk-easy
 go 1.18
 
 require (
+	github.com/getsentry/sentry-go v0.17.0
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/nhk-news-web-easy/nhk-news-fetcher-go v0.0.0-20221029032953-4d6586d16420
 )
@@ -12,4 +13,8 @@ require (
 	github.com/robfig/cron/v3 v3.0.1 // indirect
 )
 
-require golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+require (
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"github.com/getsentry/sentry-go"
 	"github.com/go-co-op/gocron"
 	"github.com/ting-app/ting-task-nhk-easy/ting"
 	"log"
+	"os"
 	"time"
 )
 
 func main() {
+	enableSentry := os.Getenv("ENABLE_SENTRY") == "true"
+	sentryDsn := os.Getenv("SENTRY_DSN")
+
+	if enableSentry {
+		if sentryDsn == "" {
+			log.Fatal("sentryDsn is required")
+		}
+
+		err := sentry.Init(sentry.ClientOptions{
+			Dsn:              sentryDsn,
+			TracesSampleRate: 1.0,
+		})
+
+		if err != nil {
+			log.Fatalf("sentry.Init: %s", err)
+		} else {
+			log.Println("sentry enabled")
+		}
+	}
+
 	scheduler := gocron.NewScheduler(time.UTC)
 	_, err := scheduler.Every(1).Day().At("10:30").Do(func() {
 		err := ting.RunTask()
 
 		if err != nil {
 			log.Printf("Run task error %v\n", err)
+
+			if enableSentry {
+				sentry.CaptureException(err)
+			}
 		}
 	})
 
